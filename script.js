62fb4a7e9dd7b0cae0946e4f855e4dd367080267
JuanCarlos92 <juancarlosfilter@gmail.com>
2024-11-07 23:41:15 +0100

    update img  perfil, lenguajes y acerca de

--- script.js
+++ script.js
@@ -32,10 +32,13 @@ function efectoHabilidades() {
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if (distancia_skills >= 300) {
-        document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("spring").classList.add("barra-progreso1");
+        document.getElementById("js-front").classList.add("barra-progreso2");
         document.getElementById("bd").classList.add("barra-progreso3");
         document.getElementById("java").classList.add("barra-progreso4");
+        document.getElementById("kotlin").classList.add("barra-progreso5");
+
+        
     }
 
 }
